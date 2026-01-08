7712366369b206131aa82691abb6810cf6fbd5aa
Rob P <29809914+RobP-git@users.noreply.github.com>
2026-01-08 13:51:16 +0700

    Add Thai flashcards web app

---
+++ words.js
@@ -0,0 +1,50 @@
+const WORD_SETS = [
+  {
+    english: "Hello",
+    thai: "สวัสดี",
+    audioIndex: 1,
+    gender: "all",
+  },
+  {
+    english: "Thank you (man speaking)",
+    thai: "ขอบคุณครับ",
+    audioIndex: 2,
+    gender: "man",
+  },
+  {
+    english: "Thank you (woman speaking)",
+    thai: "ขอบคุณค่ะ",
+    audioIndex: 2,
+    gender: "woman",
+  },
+  {
+    english: "Yes",
+    thai: "ใช่",
+    audioIndex: 3,
+    gender: "all",
+  },
+  {
+    english: "No",
+    thai: "ไม่",
+    audioIndex: 4,
+    gender: "all",
+  },
+  {
+    english: "Nice to meet you",
+    thai: "ยินดีที่ได้รู้จัก",
+    audioIndex: 5,
+    gender: "all",
+  },
+  {
+    english: "Excuse me (man speaking)",
+    thai: "ขอโทษครับ",
+    audioIndex: 6,
+    gender: "man",
+  },
+  {
+    english: "Excuse me (woman speaking)",
+    thai: "ขอโทษค่ะ",
+    audioIndex: 6,
+    gender: "woman",
+  },
+];
