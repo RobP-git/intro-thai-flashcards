3a5c292022ad65c9cd16d33f4a018961bd2abbe1
Rob P <29809914+RobP-git@users.noreply.github.com>
2026-01-08 14:42:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ function highlightSelection(gender, buttons, selectedLabel) {
   buttons.forEach((button) => {
     button.classList.toggle("active", button.dataset.gender === gender);
   });
-  selectedLabel.textContent = `Saved as: ${gender}`;
+  //selectedLabel.textContent = `Saved as: ${gender}`;
 }
 
 function enableActions(actions) {
